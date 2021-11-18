f0c8467c3352ca63bfc6f0a605e995ce7257a458
SaidMagomedow <71436061+SaidMagomedow@users.noreply.github.com>
2021-11-18 12:23:25 +0300

    Update test_views.py

--- books/main/tests/test_views.py
+++ books/main/tests/test_views.py
@@ -1,18 +1,4 @@
 from test_plus import TestCase
 
-from books.books.utils import get_authors
-
-
-class AuthorListViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.author = get_authors()
-
-    def setUp(self):
-        self.path = self.reverse('main:author_list')
-
-    def test_get(self):
-        self.get_check_200(self.path)
-
 
 

e141f64dd0ce05d8991c919cd92e67af489d50ec
SaidMagomedow <71436061+SaidMagomedow@users.noreply.github.com>
2021-11-18 12:22:02 +0300

    Update urls.py

--- books/main/urls.py
+++ books/main/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import AuthorListView, BooksListView, BookDetailView, BooksUpdateView, BooksDeleteView
+from .views import BooksListView, BookDetailView, BooksUpdateView, BooksDeleteView
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', AuthorListView.as_view(), name='author_list'),
     path('books/list/', BooksListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/update/<int:pk>/', BooksUpdateView.as_view(), name='book-update'),
